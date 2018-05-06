816e2d5895b491e0b7b21333064e017a0d1f7109
Denis Senchishen <senden1998@gmail.com>
2018-05-06 19:54:28 +0300

    Code rewrited using *using* directive anywhere

--- bank/Program.cs
+++ bank/Program.cs
@@ -12,34 +12,43 @@ namespace bank
     {
         static void Main(string[] args)
         {
-            var xmlOldBankClientSerializer = new XmlSerializer(typeof(OldBankClient));
-            var xmlNewBankClientSerializer = new XmlSerializer(typeof(NewBankClient));
-
-            var clientXmlReader = new System.IO.StreamReader("inputData/clientinfo_input.xml");
-            var clientXmlWriter = new System.IO.StreamWriter("outputData/clientinfo_output.xml");
-
-            var oldClient = xmlOldBankClientSerializer.Deserialize(clientXmlReader);
-            var newClient = new NewBankClient((OldBankClient)oldClient);
-
-            xmlNewBankClientSerializer.Serialize(clientXmlWriter, newClient);
-
-            var jsonNewBankClient = JsonConvert.SerializeObject(newClient, Formatting.Indented);
-            using (var writer = new System.IO.StreamWriter("outputData/clientinfo_output.json"))
+            // First task
+            using (var reader = new System.IO.StreamReader("inputData/clientinfo_input.xml"))
             {
-                writer.Write(jsonNewBankClient);
+                var xmlOldBankClientSerializer = new XmlSerializer(typeof(OldBankClient));
+                var xmlNewBankClientSerializer = new XmlSerializer(typeof(NewBankClient));
+
+                var oldClient = xmlOldBankClientSerializer.Deserialize(reader);
+                var newClient = new NewBankClient((OldBankClient)oldClient);
+
+                using (var writer = new System.IO.StreamWriter("outputData/clientinfo_output.xml"))
+                {
+                    xmlNewBankClientSerializer.Serialize(writer, newClient);
+                }
+
+                var jsonNewBankClient = JsonConvert.SerializeObject(newClient, Formatting.Indented);
+                using (var writer = new System.IO.StreamWriter("outputData/clientinfo_output.json"))
+                {
+                    writer.Write(jsonNewBankClient);
+                }
+
+                var workAddress = newClient.workAddress;
+                var xmlNewBankClientAddressSerializer = new XmlSerializer(typeof(Address));
+                using (var writer = new System.IO.StreamWriter("outputData/workaddress_output.xml"))
+                {
+                    xmlNewBankClientAddressSerializer.Serialize(writer, workAddress);
+                }
+
+                var homeAddress = newClient.homeAddress;
+                var jsonNewBankClientHomeAddress = JsonConvert.SerializeObject(homeAddress, Formatting.Indented);
+                using (var writer = new System.IO.StreamWriter("outputData/homeaddress_output.json"))
+                {
+                    writer.Write(jsonNewBankClientHomeAddress);
+                }
             }
 
-            var workAddress = newClient.workAddress;
-            var xmlNewBankClientAddressSerializer = new XmlSerializer(typeof(Address));
-            var clientXmlAddressWriter = new System.IO.StreamWriter("outputData/workaddress_output.xml");
-            xmlNewBankClientAddressSerializer.Serialize(clientXmlAddressWriter, workAddress);
 
-            var homeAddress = newClient.homeAddress;
-            var jsonNewBankClientHomeAddress = JsonConvert.SerializeObject(homeAddress, Formatting.Indented);
-            using (var writer = new System.IO.StreamWriter("outputData/homeaddress_output.json"))
-            {
-                writer.Write(jsonNewBankClientHomeAddress);
-            }
+
         }
     }
 }
